e7b8b22536d20f84deac1c8f0a0fda0fa345f23f
Umut <56222030+UmutYLCN@users.noreply.github.com>
2022-07-14 09:10:14 +0300

    Initial Commit

---
+++ DGSMeter/DGSMeterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DGSMeterApp.swift
+//  DGSMeter
+//
+//  Created by umut yalçın on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct DGSMeterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
